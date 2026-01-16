ba64aaa632659ef8e55c121e56d54a7489355fb8
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2026-01-16 23:27:04 +0000

    adds /api/v1/migrations endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,26 +1,38 @@
-import { Pool } from "pg";
-
-const globalForDb = globalThis;
-
-const pool =
-  globalForDb.pool ??
-  new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl:
-      process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForDb.pool = pool;
-}
+import { Client } from "pg";
 
 async function query(queryObject) {
-  const result = await pool.query(queryObject);
-  return result;
+  let client;
+
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
 }
 
 export default {
   query,
+  getNewClient,
 };
+
+function getSSLValues() {
+  return process.env.NODE_ENV === "production";
+}
